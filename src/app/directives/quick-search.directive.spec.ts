e5563e31cf9d7880cbf05594ff183040be8f6d99
molinero95 <jaime.molinero.lacave@gmail.com>
2018-11-05 00:07:57 +0100

    esto fuera

--- src/app/directives/quick-search.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { QuickSearchDirective } from './quick-search.directive';
-
-describe('QuickSearchDirective', () => {
-  it('should create an instance', () => {
-    const directive = new QuickSearchDirective();
-    expect(directive).toBeTruthy();
-  });
-});
